d2d0c56b8e3ed6f6ae52aa3f7d5e9390103bbb41
mayanknoldus <82813390+mayanknoldus@users.noreply.github.com>
2021-06-06 22:17:30 +0530

    Update companyValidityTest.scala

--- src/test/scala/companyValidityTest.scala
+++ src/test/scala/companyValidityTest.scala
@@ -1,14 +1,29 @@
+import com.knoldus.db.CompanyReadDto
 import com.knoldus.models.Company
-import com.knoldus.validator.CompanyValidator
+import com.knoldus.validator.{CompanyValidator, EmailValidator}
+import org.mockito.MockitoSugar.{mock, when}
 import org.scalatest.flatspec.AnyFlatSpec
 
-class companyValidityTest extends AnyFlatSpec{
+class CompanyValidityTest extends AnyFlatSpec {
 
-  "companyValidityTest" should "verify if company is valid or not" in {
+  val sampleCompany: Company = Company("Knoldus", "knoldus@gmail.com", "Noida")
+  val mockedCompanyReadDto: CompanyReadDto = mock[CompanyReadDto]
+  val mockedEmailValidator: EmailValidator = mock[EmailValidator]
 
-    val companyValidityObj = new CompanyValidator
-    assert(companyValidityObj.companyIsValid(Company("KnoldusSoftware", "knoldus@gmail.com", "Noida")) == true)
+  "company" should "be valid" in{
+    val companyValidator = new CompanyValidator
 
+    when(mockedCompanyReadDto.getCompanyByName(sampleCompany.name).isEmpty && mockedEmailValidator.emailIdIsValid(sampleCompany.emailId)) thenReturn true
+
+    assert(companyValidator.companyIsValid(sampleCompany))
+  }
+
+  "company" should "be invalid" in{
+    val companyValidator = new CompanyValidator
+
+    when(mockedCompanyReadDto.getCompanyByName(sampleCompany.name).isEmpty && mockedEmailValidator.emailIdIsValid(sampleCompany.emailId)) thenReturn false
+
+    assert(!companyValidator.companyIsValid(sampleCompany))
   }
 
 }
